f2bea107587d86fdcd12080f59126508569865f0
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-03-24 00:35:01 +0530

    update: select answer among options, move to next question

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { space } from "../data/questions";
+import "../styles/styles.css";
 
 const Questions = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
+  const [selected, setSelected] = useState(null);
+
   return (
     <div className="questions container">
-      <h1>What is the name of our galaxy?</h1>
+      <h1>{space[currentQuestion].que}?</h1>
+      <span>{space[currentQuestion].id}</span>
+      {space[currentQuestion].options.map((option, index) => (
+        <label
+          className={`questions__options ${
+            selected === option ? "questions__options--selected" : ""
+          }`}
+          for={option}
+          onClick={() => {
+            console.log(option);
+            setSelected(option);
+          }}
+        >
+          <span className="questions__numbers">{index + 1}</span>
+          <span for={option} className="">
+            {option}
+          </span>
+          <input
+            type="radio"
+            id={option}
+            name={space[currentQuestion].ans}
+            value={space[currentQuestion].ans}
+            // selected={selected === option}
+          />
+        </label>
+      ))}
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          setSelectedAnswers([...selectedAnswers, selected]);
+          setCurrentQuestion(currentQuestion + 1);
+          setSelected(null);
+        }}
+        disabled={currentQuestion === space.length - 1}
+      >
+        Next
+      </button>
     </div>
   );
 };
---
+++ src/data/questions.js
@@ -0,0 +1,32 @@
+export const space = [
+  {
+    id: 1,
+    que: "What is the name of out galaxy",
+    options: ["Milky way", "Andromeda", "Abc", "bahhh"],
+    ans: "Milky way",
+  },
+  {
+    id: 2,
+    que: "What is the name of out galaxy",
+    options: ["Milky way", "Andromeda", "Abc", "bahhh"],
+    ans: "Milky way",
+  },
+  {
+    id: 3,
+    que: "What is the name of out galaxy",
+    options: ["Milky way", "Andromeda", "Abc", "bahhh"],
+    ans: "Milky way",
+  },
+  {
+    id: 4,
+    que: "What is the name of out galaxy",
+    options: ["Milky way", "Andromeda", "Abc", "bahhh"],
+    ans: "Milky way",
+  },
+  {
+    id: 5,
+    que: "What is the name of out galaxy",
+    options: ["Milky way", "Andromeda", "Abc", "bahhh"],
+    ans: "Milky way",
+  },
+];
