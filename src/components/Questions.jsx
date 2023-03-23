6d4925274d4eeb9b75100fd686069b42c2087219
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-03-23 22:44:42 +0530

    add: Questions component

---
+++ src/components/Questions.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Questions = () => {
+  return (
+    <div className="questions container">
+      <h1>What is the name of our galaxy?</h1>
+    </div>
+  );
+};
+
+export default Questions;
